5f4d2106acc123eb60be01c4f658ca247f31e549
Yogesh Khutwad <khutwadyogesh34@gmail.com>
2024-12-25 00:28:01 +0530

    update Interfont file

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -12,7 +12,7 @@ export const contentType = "image/png";
 
 export default async function Image() {
   const interSemiBold = await fetch(
-    new URL("./Inter-SemiBold.ttf", import.meta.url)
+    new URL("./Inter_18pt-SemiBold.ttf", import.meta.url)
   ).then((res) => res.arrayBuffer());
 
   return new ImageResponse(
